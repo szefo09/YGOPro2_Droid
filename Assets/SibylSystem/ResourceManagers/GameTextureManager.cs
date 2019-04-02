2c8c6f7741ab290a7e8081a1557f252bdfa88b59
purerosefallen <78877@qq.com>
2019-04-02 18:05:51 +0800

    change download path

--- Assets/SibylSystem/ResourceManagers/GameTextureManager.cs
+++ Assets/SibylSystem/ResourceManagers/GameTextureManager.cs
@@ -817,8 +817,8 @@ public class GameTextureManager
             if (!File.Exists(path) && pic.code != 0 && AutoPicDownload)
             {
                 //YGOMobile (177x254)
-                df.Download("http://koishi.222diy.gdn/smallpics/" + pic.code.ToString() + ".jpg", "expansions/pics/" + pic.code.ToString() + ".jpg");
-                path = "expansions/pics/" + pic.code.ToString() + ".jpg";
+                df.Download("http://koishi.222diy.gdn/smallpics/" + pic.code.ToString() + ".jpg", "picture/card/" + pic.code.ToString() + ".jpg");
+                path = "picture/card/" + pic.code.ToString() + ".jpg";
             }
             /*if (!File.Exists(path) && pic.code != 0 && AutoPicDownload)
             {
