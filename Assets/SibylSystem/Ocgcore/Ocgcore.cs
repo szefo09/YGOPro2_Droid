e49b14979881297d3dd9c2596f7aa06caa7445a5
mercury233 <me@mercury233.me>
2019-04-06 03:24:49 +0800

    fix version

--- Assets/SibylSystem/Ocgcore/Ocgcore.cs
+++ Assets/SibylSystem/Ocgcore/Ocgcore.cs
@@ -8611,14 +8611,7 @@ public class Ocgcore : ServantWithCardDescription
                         BinaryMaster binaryMaster = new BinaryMaster();
                         for (int i = 0; i < allCardsInSelectMessage.Count; i++)
                         {
-                            if (Config.ClientVersion>=0x133d)   
-                            {
-                                binaryMaster.writer.Write((short)allCardsInSelectMessage[i].counterSELcount);
-                            }
-                            else
-                            {
-                                binaryMaster.writer.Write((byte)allCardsInSelectMessage[i].counterSELcount);
-                            }
+                            binaryMaster.writer.Write((short)allCardsInSelectMessage[i].counterSELcount);
                         }
                         sendReturn(binaryMaster.get());
                     }
