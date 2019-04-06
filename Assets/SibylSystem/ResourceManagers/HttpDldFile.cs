d0397c23a9f0c99d39487a8dde6a25f11e5fb121
purerosefallen <78877@qq.com>
2019-04-06 18:20:26 +0800

    shorten

--- Assets/SibylSystem/ResourceManagers/HttpDldFile.cs
+++ Assets/SibylSystem/ResourceManagers/HttpDldFile.cs
@@ -34,7 +34,7 @@ public class HttpDldFile
                 }
                 if (Path.GetExtension(filename).Contains("cdb"))
                 {
-                    client.Timeout = 15000;
+                    client.Timeout = 5000;
                 }
                 if (Path.GetExtension(filename).Contains("conf"))
                 {
