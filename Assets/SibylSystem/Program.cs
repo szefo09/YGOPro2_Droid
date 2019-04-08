f38ee3d353d5f918566a2141150cfab6fcbf4ca3
purerosefallen <78877@qq.com>
2019-04-08 21:45:02 +0800

    version in corner

--- Assets/SibylSystem/Program.cs
+++ Assets/SibylSystem/Program.cs
@@ -1064,7 +1064,7 @@ public class Program : MonoBehaviour
             _padScroll = 0;
         }
 
-        GUI.Label(new Rect(10, 5, 200, 200), "[Ver 2.2.4] " + "FPS: " + m_FPS);
+        GUI.Label(new Rect(10, 5, 200, 200), "[Ver 1.034.9-6] " + "FPS: " + m_FPS);
     }
 
     void Update()
