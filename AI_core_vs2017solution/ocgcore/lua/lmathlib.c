e5126aafeb30484345f006665789201e73faac1a
Unicorn369 <3038572935@qq.com>
2019-04-27 12:48:35 +0800

    fix

--- AI_core_vs2017solution/ocgcore/lua/lmathlib.c
+++ AI_core_vs2017solution/ocgcore/lua/lmathlib.c
@@ -184,8 +184,11 @@ static int math_log (lua_State *L) {
   else {
     lua_Number base = luaL_checknumber(L, 2);
 #if !defined(LUA_USE_C89)
+#if __ANDROID__
+#else
     if (base == l_mathop(2.0))
       res = l_mathop(log2)(x); else
+#endif
 #endif
     if (base == l_mathop(10.0))
       res = l_mathop(log10)(x);
